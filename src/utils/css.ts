29ca0b5b4a9a51aa7fda20d08e9c6581d642c166
Roovers <91910203+Roovers@users.noreply.github.com>
2026-06-27 23:40:39 -0300

    Subir version final dashboard seguridad

---
+++ src/utils/css.ts
@@ -0,0 +1,7 @@
+export const normalizarClase = (value: string) =>
+  value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "");
